79487763bacc4f92069ebbf2f7b9886655bed4cb
Haroun Meghaichi <haroun.meghaichi@gmail.com>
2024-01-02 14:07:46 -0500

    circle and sphere tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,15 @@ using Test
 
 @testset "QuadratureOnImplicitRegions.jl" begin
     # Write your tests here.
+    ψ(x)=x'*x-1
+
+    #quarter of a circle
+    a,b=zeros(2), ones(2)
+    x,w=algoim_nodes_weights(ψ,-1.0, a,b,10)
+    @test sum(w) ≈ π/4
+
+    #1/8 of a sphere
+    a,b=zeros(3), ones(3)
+    x,w=algoim_nodes_weights(ψ,-1.0, a,b,10)
+    @test sum(w) ≈ π/6
 end
