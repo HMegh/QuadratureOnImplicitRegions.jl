c7dfe545760bdeb0f4b9120260bac0ee761a31fa
Haroun Meghaichi <haroun.meghaichi@gmail.com>
2024-06-11 20:31:18 -0400

    clean up the code (and allocation improvements)

--- src/QuadratureOnImplicitRegions.jl
+++ src/QuadratureOnImplicitRegions.jl
@@ -1,15 +1,15 @@
 module QuadratureOnImplicitRegions
 
 
-using FastGaussQuadrature
+using FastGaussQuadrature,LinearAlgebra,ForwardDiff
 
 
 export algoim_nodes_weights
-export algoim_quad
+# export algoim_quad #nneds work
 
 include("algoim.jl")
 
-include("integrate_f.jl")
+# include("integrate_f.jl") #Needs some work
 include("algoim_functions.jl")
 
 
--- src/algoim_functions.jl
+++ src/algoim_functions.jl
@@ -1,79 +1,156 @@
-using LinearAlgebra
-using FastGaussQuadrature
+sgn(m,s,σ)= ( m==σ*s ? σ*m : 0.0)
 
-sgn=(m,s,σ)-> m==σ*s ? σ*m : 0;
 
-function findroots(ψ_list,domain)
-    #Returns a list of union of all zeros of psi_i in  domain and the
-    #boundaries of the domain (for convenience). 
+"""
+    find_root(ψ::F,a::T,b::T) 
     
-    a,b=domain;
-    Z=[a,b];
+Finds a root of a function ψ in the open interval `(a,b)`
+"""
+function find_root(ψ::F,a::T,b::T) where {F,T}
+    if ψ(a)*ψ(b)≥0 error("Find_root failed: Choose a smaller interval") end 
+
+    c=b
+    cnext=(a+b)/2
+    while !(c≈cnext) 
+        c=cnext
+        cnext=cnext-ψ(cnext)/ForwardDiff.derivative(ψ,cnext)
+    end
+    return c
+end
+
+"""
+    find_roots(ψ::F,a::T,b::T) where {F,T}
+
+Returns multiple roots of ψ in the open interval `[a,b]`
+"""
+function find_roots(ψ::F,a::T,b::T) where {F,T}
+
+    n=20 #split the interval [a,b] into n sub-intervals
+    h=(b-a)/n
+    Z=Vector{Float64}(undef,0)
+    for i=0:n-1
+        if ψ(a+i*h)*ψ(a+(i+1)*h)<0 
+            z=find_root(ψ,a+i*h,a+(i+1)*h)
+            push!(Z,z)
+        end
+    end
+    
+    for i=1:n if ψ(a+i*h)==0 push!(Z,a+i*h) end end 
+
+    sort!(Z)
+    return Z
+
+end
 
 
-    Xsamples=range(a,b,20);
+"""
+    find_roots(ψ_list::Vector{F},a::T,b::T) where {F,T}
+
+Returns multiple roots of multiple functions ψ in the closed interval `[a,b]`, it includes the endpoints by default even if they are not roots. 
+"""
+function find_roots(ψ_list::Vector{F},a::T,b::T) where {F,T}
+
+    n=20 #split the interval [a,b] into n sub-intervals
+    h=(b-a)/n
+    Z=[a,b]
     for ψ in ψ_list
-       
-        samples=ψ.(Xsamples);
-
-        
-
-        indx=findall(samples[1:end-1] .* samples[2:end] .<0);
-        
-        zer=
-        [bisection(ψ,Xsamples[kk],Xsamples[kk+1])
-            for kk in indx];
-
-
-        Z=vcat(Z,zer);
-
+        zer=find_roots(ψ,a,b)
+        append!(Z,zer)
     end
-    return sort(Z);
-end
-function bisection(f,a::T,b::T) where T<:Union{Float16,Float32,Float64}
-
-
-    p=f(a);q=f(b);
-
-    #see if the zero is at the endpoints.
-    if p==0 return a;end
-    if q==0 return b; end;
-
-    if p*q>0 return; end
-
-   
-
-    while abs(b-a)>eps(one(T))
-        c=(a+b)/2;r=f(c);
-
-
-
-        if r*p<0 
-             b=c;q=r;
-         else
-             a=c;p=r; 
-         end
-         
-        if abs(p)<eps(one(T)) return a end 
-        if abs(q)<eps(one(T)) return b end
-    end
-
-    return (a+b)/2;
+    sort!(Z)
+    Z
 end
 
+"""
+    shifted_gl(x_ref::Vector{T},w_ref::Vector{T},a::T,b::T)
 
-
-
-function lgwtjl(N::Int,a::T,b::T) where T<:Union{Float16,Float32,Float64}
-    xref,wref=gausslegendre(N)
-    x=Vector{T}(undef,N)
-    w=Vector{T}(undef,N)
-    x.=(b-a)/2*xref .+(a+b)/2
-    w.=(b-a)/2*wref
+Shifts the Gauss-Legendre rule (x_ref,w_ref) from `(-1,1)` to the interval [a,b]
+"""
+function shifted_gl(x_ref::Vector{T},w_ref::Vector{T},a::T,b::T) where T
+    x,w=similar(x_ref),similar(w_ref)
+    for i in eachindex(x) x[i]=(a+b)/2+(b-a)/2*x_ref[i] end
+    for i in eachindex(w) w[i]=(b-a)/2*w_ref[i] end
     return x,w
 end
 
-function linsamples_creator(U::Matrix{T},n::Int) where T<:Union{Float16,Float32,Float64}
+"""
+    shifted_gl!(x_ref::Vector{T},w_ref::Vector{T},a::T,b::T,x::Vector{T},w::Vector{T})
+
+Shifts the Gauss-Legendre rule (x_ref,w_ref) from `(-1,1)` to the interval [a,b] and stores them in x,w.
+"""
+function shifted_gl!(x_ref::Vector{T},w_ref::Vector{T},a::T,b::T,x::Vector{T},w::Vector{T}) where T
+    for i in eachindex(x) x[i]=(a+b)/2+(b-a)/2*x_ref[i] end
+    for i in eachindex(w) w[i]=(b-a)/2*w_ref[i] end
+    return 
+end
+
+
+"""
+    d1_case(ψ_list,s_list,domain,x_ref,w_ref)
+
+    Returns a quadrature rule on the subset where s_i*ψ_i<0 (see the paper).
+"""
+function  d1_case(ψ_list,s_list,domain,x_ref,w_ref)
+    Z=find_roots(ψ_list,domain[1],domain[2])
+    x=Vector{eltype(x_ref)}(undef,0)
+    w=Vector{eltype(w_ref)}(undef,0)
+
+    xloc=similar(x_ref)
+    wloc=similar(w_ref)
+
+    n=length(Z)
+    for i=1:n-1
+        a=Z[i];b=Z[i+1];c=(a+b)/2
+
+        flag=true 
+        for i in eachindex(ψ_list) 
+            if s_list[i]*ψ_list[i](c)<0
+                flag=false;break;
+            end
+        end    
+        if flag
+            shifted_gl!(x_ref,w_ref,a,b,xloc,wloc)
+            append!(x,xloc)
+            append!(w,wloc)
+        end
+    end 
+
+    return reshape(x,1,:),w
+end
+
+
+"""
+    tensor_GL_rule(U::Matrix{T},x_ref::Vector{T},w_ref::Vector{T})
+
+    Returns a matrix x and a vector w (multidimensional quadrature) 
+
+    Credit for the idea: 
+    https://discourse.julialang.org/t/about-storing-the-results-of-iterators-product-into-an-array-efficiently/115504
+"""
+function tensor_GL_rule(U::Matrix{T},x_ref::Vector{T},w_ref::Vector{T}) where T
+    d=size(U,2)
+    n=size(x_ref,1)
+
+    X=[(U[1,i]+U[2,i])/2 .+ x_ref .* (U[2,i]-U[1,i])/2 for i=1:d]
+    W=[ w_ref/2 .* (U[2,i]-U[1,i]) for i=1:d]
+
+    x_tuples=collect(Iterators.product(X...))
+
+    x=Matrix(reshape(reinterpret(T, x_tuples), d, n^d))
+
+    
+    w=[prod(A) for A in collect(Iterators.product(W...))[:]]
+
+    return x,w
+end
+
+"""
+    linsamples_creator(U::Matrix{T},n::I) where {T,I}
+    
+    returns sample points in the cuboid U (as columns).
+"""
+
+function linsamples_creator(U::Matrix{T},n::I) where {T,I}
     d=size(U,2);
 
     #linspaces in each direction
@@ -82,91 +159,33 @@ function linsamples_creator(U::Matrix{T},n::Int) where T<:Union{Float16,Float32,
     
     #Iterators.product returns the cartesian product as an object,
     # collect makes it a matrix and [:] makes it a vector of tuples. 
-    x= collect(Iterators.product(lins...))[:];
+    x_tuples= collect(Iterators.product(lins...))
 
 
-    x=[[y for y in z] for z in x];
+    return Matrix(reshape(reinterpret(T, x_tuples), d, n^d))
     
-end   
+end  
 
 
-function tensor_GL_rule(U::Matrix{T},q::Int) where T<:Union{Float16,Float32,Float64}
+# remove_kth(x::Vector,k::Integer)=vcat(x[1:k-1],x[k+1:end]);
+# remove_kth(x::Number,k::Integer)=Vector{typeof(x)}(undef,0); 
 
-
-    d=size(U,2);
-    xref, wref=lgwtjl(q,zero(T),one(T));
-
-    X=[U[1,i] .+ xref .* (U[2,i]-U[1,i]) for i=1:d]; 
-    W=[ wref .* (U[2,i]-U[1,i]) for i=1:d]; 
-
-    #Iterators.product returns the cartesian product as an object,
-    # collect makes it a matrix and [:] makes it a vector of tuples. 
-    x=collect(Iterators.product(X...))[:];
-
-    x=[[t for t in y] for y in x]
-
-    w=[prod(A) for A in collect(Iterators.product(W...))[:]];
-
-    return x,w
-end
-function d1_case(ψ_list,s_list::Vector{T},domain::Array{T},q::Int) where T<:Union{Float16,Float32,Float64}
-
-
-
-
-    #The one-dimensional case. s_list and psi_list
-    # have the same length
-    zer=findroots(ψ_list,domain);
-    x=Vector{T}(undef,0);
-    w=Vector{T}(undef,0);
-
-    for i=1:length(zer)-1
-        a=zer[i];b=zer[i+1];c=(a+b)/2;
-        #check to see if s_i*psi_i(c)>0 for all i      
-        #I used count: if there is an instance where 
-        # psi_i(c)*s_i<0, then flag is 0. 
-
-        flag=true
-
-        # flag=count([s_list[i]*ψ_list[i](c)<0 
-        #         for i=1:length(s_list)])==0;
-
-        for i=1:length(s_list) 
-            if s_list[i]*ψ_list[i](c)<0 flag=false end
-        end
-
-
-        if flag #psi_i(c)s_i>0 for all i
-
-            xx,ww=lgwtjl(q,a,b);
-            x=vcat(x,xx);
-            w=vcat(w,ww);
-        end
-    end
-    
-
-    return x,w
-    
+function remove_kth(x::V,k::I) where {I,V<:AbstractVector}
+    return deleteat!(copy(x),k)
 end
 
+function remove_kth(x::T,k::I) where {I,T<:Number}
+    return Vector{T}(undef,0)
+end
 
+function insert_kth(x::V,k::I,t::T) where {I,V<:AbstractVector,T<:Number}
+    [x[1:k-1];t;x[k:end]]
+end
 
-remove_kth(x::Vector,k::Integer)=vcat(x[1:k-1],x[k+1:end]);
-remove_kth(x::Number,k::Integer)=Vector{typeof(x)}(undef,0); 
-
-#tilde insert takes  [1,2,3,4],3,pi and returns [1,2,pi,3,4]
-#there is a builtin function insert! but it does not work with scalars
-## 
-insert_kth(x::Vector,k::Integer,t::Number)=vcat(x[1:k-1],t,x[k:end]);
-function insert_kth(x::Number,k::Integer,t::Number)
+function insert_kth(x::N,k::I,t::S) where {N<:Number,I,S<:Number}
     if k==1
         return [t,x]
     elseif k==2
         return [x,t]
     end
 end
-
-
-
-
-
